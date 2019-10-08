09663f79ba61c57b85b7ce73528a6716e8fc70be
VasylievHennadii <vga.kha@gmail.com>
2019-10-08 12:29:49 +0300

    hw2 add dignity cards

--- hw-2/formHw2.php
+++ hw-2/formHw2.php
@@ -1,12 +1,28 @@
 <?php
-
+$mass = array();
+$mass['joker'] = 'the face value of the card is the largest - he is the president';
+$mass['6'] = [6, 1];
+$mass['7'] = 7;
+$mass['8'] = 8;
+$mass['9'] = 9;
+$mass['10'] = 10;
+$mass['Jack'] = 11;
+$mass['Queen'] = 12;
+$mass['King'] = 13;
+$mass['Ace'] = 14;
 if(!empty($_POST)) {
   if($_POST['suit'] == 'joker' || $_POST['dignity'] == 'joker') {
     echo '<h2>Joker has already become president, so make your choice in favor of another candidate</h2>';    
-    echo '<img src="/img/jokerjoker.jpg" style="width: 25%;">';
+    echo '<img src="/img/jokerjoker.jpg" style="width: 20%;">';
   } else {
-    echo '<img src="/img/'. "$_POST[suit]" . "$_POST[dignity]" . '.jpg" style="width: 25%;">';
+    echo '<img src="/img/'. "$_POST[suit]" . "$_POST[dignity]" . '.jpg" style="width: 20%;">';
   }  
+    echo '<br><br><br>';
+    $n = $_POST['dignity'];
+    if($_POST['suit'] !== 'joker' || $_POST['dignity'] == 'joker') {
+      echo "<h3>dignity card is : $mass[$n]</h3>";
+    }
+    
 }
 
 ?>
